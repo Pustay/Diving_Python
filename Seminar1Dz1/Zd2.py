4af0d2941c0dd7036bf23c429926c4f1c21632a7
Natasha <115388396+Pustay@users.noreply.github.com>
2023-08-08 13:25:02 +0900

    Add files via upload

--- Seminar1Dz1/Zd2.py
+++ Seminar1Dz1/Zd2.py
@@ -1,15 +1,52 @@
-# Напишите код, который запрашивает число и сообщает является ли оно простым или составным.
-# Используйте правило для проверки: «Число является простым, если делится нацело только 
-# на единицу и на себя». Сделайте ограничение на ввод отрицательных чисел и чисел больше 100 тысяч.
+# Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем.
+# Программа должна возвращать сумму и произведение* дробей.
+# Для проверки своего кода используйте модуль fractions.
 
-num = int(input("Введите число: "))
+from fractions import Fraction
 
-if num <= 1 or num > 100000:
-    print("Число должно быть в диапазоне от 2 до 100000.")
-else:
-    simple = True
-    for i in range(2, num):
-        if num % i == 0:
-            simple = False
-            break
-    print('Простое число' if simple else 'Составное число')
+class SelfFraction:
+    def __init__(myself, numerator: int, denominator: int):
+        if not isinstance(numerator, int) and not isinstance(denominator, int):
+            raise ValueError
+        elif denominator == 0:
+            raise ZeroDivisionError
+        else:
+            nod = SelfFraction.check_nod(numerator, denominator)
+            myself.num = numerator // nod
+            myself.den = denominator // nod
+
+    def __add__(myself, other):
+        main_den = myself.den * other.den
+        main_num = myself.num * other.den + other.num * myself.den
+        return SelfFraction(main_num, main_den)
+
+    def __mul__(myself, other):
+        main_num = myself.num * other.num
+        main_den = myself.den * other.den
+        return SelfFraction(main_num, main_den)
+
+    @staticmethod
+    def check_nod(num: int, den: int) -> int:
+        nod = 1
+        for i in range(1, max(num, den) // 2 + 1):
+            if num % i == 0 and den % i == 0:
+                nod = i
+        return nod
+
+    def __str__(myself):
+        return f'{myself.num}/{myself.den}'
+
+
+fract_1 = input('Введите первую дробь формата a/b: ').split('/')
+fract_2 = input('Введите вторую дробь формата a/b: ').split('/')
+
+myself_fract_1 = SelfFraction(int(fract_1[0]), int(fract_1[1]))
+myself_fract_2 = SelfFraction(int(fract_2[0]), int(fract_2[1]))
+original_fract_1 = Fraction(int(fract_1[0]), int(fract_1[1]))
+original_fract_2 = Fraction(int(fract_2[0]), int(fract_2[1]))
+
+print(f'Сумма дробей -> {myself_fract_1 + myself_fract_2}')
+print(f'Проверка -> {original_fract_1 + original_fract_2}')
+
+print(f'Произведение дробей -> {myself_fract_1 * myself_fract_2}')
+print(f'Проверка -> {original_fract_1 * original_fract_2}')
