40218a98e77b50396629fda219251166bc4c5c7a
Natasha <115388396+Pustay@users.noreply.github.com>
2023-09-04 14:34:50 +0900

    Add files via upload

---
+++ Seminar6Dz6/teck_1.py
@@ -0,0 +1,8 @@
+# В модуль с проверкой даты  добавьте возможность запуска в терминале с передачей даты на проверку. .
+
+from Seminar6Dz6.module_task_1.py import calend, calend_terminal
+
+
+if __name__ == '__main__':
+    print(calend('30.12.2045'))
+    calend_terminal()
